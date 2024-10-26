19e2db551dd8d45cb1a51527f68b61a6c85b0a8d
an-sangmoon <tkdans312@gmail.com>
2024-10-26 23:59:26 +0900

    mobile navbar

--- src/main.js
+++ src/main.js
@@ -19,3 +19,30 @@ const homeHeight = home.offsetHeight
 document.addEventListener('scroll', ()=>{
     home.style.opacity = 1 - window.scrollY / homeHeight;
 })
+
+
+// Arrow up  처음에 투명에서 아래로 내리면 생김
+
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+})
+
+// 모바일 NavBar 토글버튼
+
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+
+navbarToggle.addEventListener('click' , ()=>{
+    navbarMenu.classList.toggle('open');
+})
+
+// 모바일 navbar 클릭 시 자동으로 메뉴 닫음
+
+navbarMenu.addEventListener('click' , ()=>{
+    navbarMenu.classList.remove('open');
+})
